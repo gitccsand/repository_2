b076594e99bae0f0d78a250675348bffec2ce1f1
gitccsand <gozilla@163.com>
2014-10-24 14:12:42 +0800

    add code

---
+++ code/night-stack.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+./b.sh
+rv=$?
+#echo $rv
+while [[ $rv -ne 0 ]]
+do
+	echo return value is $rv
+
+	sleep 10
+#	./a.sh
+#	./b.sh
+	./devstack/unstack.sh >/dev/null
+	#./unstack.sh
+	sleep 10
+	./devstack/stack.sh >/dev/null
+
+	rv=$?
+done
+
+exit 0
